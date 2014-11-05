4611e402b2b22297fa6f2cff42feac688ed316a4
vagrant <vagrant@lighthouse-labs>
2014-11-05 16:02:30 +0000

    List all, create, delete working. TA refactored beyond my understanding tho.

--- sinatra-skeleton/app/actions.rb
+++ sinatra-skeleton/app/actions.rb
@@ -1,4 +1,34 @@
+require "sinatra/json"
 # Homepage (Root path)
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts = Contact.all
+  json @contacts
+end
+
+post '/contacts' do
+  @contact = Contact.new(
+    firstname:  params[:firstname],
+    lastname:   params[:lastname],
+    email:      params[:email],
+    phone:      params[:phone]
+    )
+  @contact.save!
+  json @contact
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find params[:id]
+  json @contacts
+end
+
+post '/contacts/:id' do
+  @contact = Contact.find params[:id]
+  @contact.destroy
+  json @contact
+end
+
+# patch
--- sinatra-skeleton/public/javascript/application.js
+++ sinatra-skeleton/public/javascript/application.js
@@ -1,4 +1,85 @@
-$(document).ready(function() {
+$(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // $("#submit_button").on("click", function(e) {
+  //   e.stopPropagation();
+  //   e.preventDefault();
+  //   submit();
+  // });
+
+  function submit(e)
+  {
+    e.stopPropagation(); //Browser, do not submit
+    e.preventDefault();
+
+    $.post('/contacts', $(this).serialize())
+      .then(function onSuccess(data)
+      {
+        console.log('Success!', data);
+        add_contact_to_table(data);
+      }, function onError(error)
+      {
+        console.error("Error :(", error);
+      });
+
+    // $.post('/contacts', { 
+    //   "firstname": $("#firstname").val(), 
+    //   "lastname": $("#lastname").val(),
+    //   "email": $("#email").val(),
+    //   "phone": $("#phone").val()
+    // }, function(data)
+    //   {
+    //   if (data.result) {
+    //     alert("Successfully created ID #" + data.id);
+    //   }
+    // }, 'json');
+  };
+
+  function delete_contact(id) {
+    $.post('/contacts/' + id, {
+      }, function(data) {
+      }, 'json');
+  };
+
+  function add_contact_to_table(contact)
+  {
+    $(contactTemplate(contact)).appendTo("#contacts");
+  }
+
+  $('#new_contact_form').on('submit', submit);
+
+  $("#contacts").on("click", "button.gm-delete", function() {
+    var id = $(this).data('contact-id');
+    delete_contact(id);
+    $(this).parent().parent().fadeOut();
+  });
+
+  var contactTemplate = _.template(
+    "<tr>" + 
+    "<td><%= firstname %></td>" +
+    "<td><%= lastname %></td>" +
+    "<td><%= email %></td>" +
+    "<td><%= phone %></td>" +
+    "<td><button class='btn btn-danger gm-edit' data-contact-id=\"<%= id %>\">Edit</button></td>"+
+    "<td><button class='btn btn-danger gm-delete' data-contact-id=\"<%= id %>\">Delete</button></td>"+
+    "</tr>");
+
+  // ' \' '
+  // " \" "
+
+  // $.getJSON('/contacts', function(data) {
+  //   $.each(data, function(index, value) {
+  //     add_contact_to_table(value);
+  //   });
+  // });
+
+  $.getJSON('/contacts', function(data)
+  {
+    data.forEach(add_contact_to_table);
+  });
+
 });
+
+// also want to:
+// 1. edit a contact
+// 2. delete a contact
+// 3. find a contact
